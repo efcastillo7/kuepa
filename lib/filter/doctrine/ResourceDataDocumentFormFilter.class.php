7ed0d3c88fa0bdaf98814b1d82c4e225790cb830
Diego Mazzocchi <diego.mazzocchi@teracode.com>
2013-07-30 19:48:07 -0300

    Add basic resource structure

---
+++ lib/filter/doctrine/ResourceDataDocumentFormFilter.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * ResourceDataDocument filter form.
+ *
+ * @package    kuepa
+ * @subpackage filter
+ * @author     fiberbunny
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ResourceDataDocumentFormFilter extends BaseResourceDataDocumentFormFilter
+{
+  /**
+   * @see ResourceDataFormFilter
+   */
+  public function configure()
+  {
+    parent::configure();
+  }
+}
---
+++ lib/form/doctrine/ResourceDataVideoForm.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * ResourceDataVideo form.
+ *
+ * @package    kuepa
+ * @subpackage form
+ * @author     fiberbunny
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class ResourceDataVideoForm extends BaseResourceDataVideoForm
+{
+  /**
+   * @see ResourceDataForm
+   */
+  public function configure()
+  {
+    parent::configure();
+  }
+}
