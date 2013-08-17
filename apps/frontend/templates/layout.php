1837d5e6818aa423f64e83212bae02b69041f6b3
Pablo <pablo@kibind.com>
2013-08-17 16:33:44 -0300

    change positions of js

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,125 +14,20 @@
         <link href="/styles/css/plataforma.css" rel="stylesheet">
         <link href="/styles/css/thumbgrid.css" rel="stylesheet">
 
-        <?php include_http_metas() ?>
-        <?php include_metas() ?>
-        <?php include_title() ?>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <?php include_stylesheets() ?>
-        <?php include_javascripts() ?>
-
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+        <script src="/js/jquery.knob.js"></script>
 
         <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
           <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
-        <script src="/js/jquery.knob.js"></script>
-        <script>
-            $(function() {
-
-                $(".knob").knob({
-                    change: function(value) {
-                        //console.log("change : " + value);
-                    },
-                    release: function(value) {
-                        //console.log(this.$.attr('value'));
-                        console.log("release : " + value);
-                    },
-                    cancel: function() {
-                        console.log("cancel : ", this);
-                    },
-                    draw: function() {
-
-                        // "tron" case
-                        if (this.$.data('skin') == 'tron') {
-
-                            var a = this.angle(this.cv)  // Angle
-                                    , sa = this.startAngle          // Previous start angle
-                                    , sat = this.startAngle         // Start angle
-                                    , ea                            // Previous end angle
-                                    , eat = sat + a                 // End angle
-                                    , r = 1;
-
-                            this.g.lineWidth = this.lineWidth;
-
-                            this.o.cursor
-                                    && (sat = eat - 0.3)
-                                    && (eat = eat + 0.3);
-
-                            if (this.o.displayPrevious) {
-                                ea = this.startAngle + this.angle(this.v);
-                                this.o.cursor
-                                        && (sa = ea - 0.3)
-                                        && (ea = ea + 0.3);
-                                this.g.beginPath();
-                                this.g.strokeStyle = this.pColor;
-                                this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sa, ea, false);
-                                this.g.stroke();
-                            }
-
-                            this.g.beginPath();
-                            this.g.strokeStyle = r ? this.o.fgColor : this.fgColor;
-                            this.g.arc(this.xy, this.xy, this.radius - this.lineWidth, sat, eat, false);
-                            this.g.stroke();
-
-                            this.g.lineWidth = 2;
-                            this.g.beginPath();
-                            this.g.strokeStyle = this.o.fgColor;
-                            this.g.arc(this.xy, this.xy, this.radius - this.lineWidth + 1 + this.lineWidth * 2 / 3, 0, 2 * Math.PI, false);
-                            this.g.stroke();
-
-                            return false;
-                        }
-                    }
-                });
-
-                // Example of infinite knob, iPod click wheel
-                var v, up = 0, down = 0, i = 0
-                        , $idir = $("div.idir")
-                        , $ival = $("div.ival")
-                        , incr = function() {
-                    i++;
-                    $idir.show().html("+").fadeOut();
-                    $ival.html(i);
-                }
-                , decr = function() {
-                    i--;
-                    $idir.show().html("-").fadeOut();
-                    $ival.html(i);
-                };
-                $("input.infinite").knob(
-                        {
-                            min: 0
-                                    , max: 20
-                                    , stopper: false
-                                    , change: function() {
-                                if (v > this.cv) {
-                                    if (up) {
-                                        decr();
-                                        up = 0;
-                                    } else {
-                                        up = 1;
-                                        down = 0;
-                                    }
-                                } else {
-                                    if (v < this.cv) {
-                                        if (down) {
-                                            incr();
-                                            down = 0;
-                                        } else {
-                                            down = 1;
-                                            up = 0;
-                                        }
-                                    }
-                                }
-                                v = this.cv;
-                            }
-                        });
-            });
-        </script>
-
+        <?php include_http_metas() ?>
+        <?php include_metas() ?>
+        <?php include_title() ?>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <?php include_stylesheets() ?>
+        <?php include_javascripts() ?>
     </head>
 
     <body>
