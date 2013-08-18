3f6ca74bb0aebe4c10e40834b0c02ca2981d31d2
iandrono <ivan@kibind.com>
2013-08-18 18:35:52 -0300

    added next resource button on lesson page

--- lib/model/doctrine/Lesson.class.php
+++ lib/model/doctrine/Lesson.class.php
@@ -10,8 +10,8 @@
  * @author     fiberbunny
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-class Lesson extends BaseLesson
-{
+class Lesson extends BaseLesson {
+
     /**
      * 
      * @return LessonTable
@@ -19,4 +19,20 @@ class Lesson extends BaseLesson
     public static function getRepository() {
         return Doctrine_Core::getTable('Lesson');
     }
+
+    public function getNextResource($previous_resource_id) {
+        $previous_learning_path = LearningPath::getRepository()->createQuery("lp")
+                ->where("lp.parent_id = ?", $this->getId())
+                ->andWhere("lp.child_id = ?", $previous_resource_id)
+                ->limit(1)
+                ->fetchOne();
+        
+        return Resource::getRepository()->createQuery("r")
+                ->innerJoin("r.LearningPath lp on r.id=lp.child_id")
+                ->where("lp.parent_id = ?", $this->getId())
+                ->andWhere("lp.position > ?", $previous_learning_path->getPosition())
+                ->limit(1)
+                ->fetchOne();
+    }
+
 }
