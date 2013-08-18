81d97b6f6bc6bf24f15363f6cab33387c20e0c6d
iandrono <ivan@kibind.com>
2013-08-18 20:22:50 -0300

    fixed circles middle

--- apps/frontend/modules/views/templates/_navigator/chapter_list.php
+++ apps/frontend/modules/views/templates/_navigator/chapter_list.php
@@ -4,18 +4,20 @@
     </ul>
     <ul class="unstyled">
         <?php foreach ($chapters as $chapter): ?>
+            <?php $current_percentage = ProfileComponentCompletedStatusService::getInstance()->getCompletedStatus($profile->getId(), $chapter->getId()) ?>
         <li>
             <a href="<?php echo url_for("chapter/expanded?chapter_id=" . $chapter->getId() . "&course_id=" . $course->getId()) ?>" class="remote-link">
                 <div class="lp-node">
-                    <div class="lp-bar-prev"></div>
-                    <div class="lp-bar-post viewed"></div>
+                    <div class="lp-bar-prev <?php echo ($previous_percentage!=null&&$previous_percentage==100?"viewed":"") ?>"></div>
+                    <div class="lp-bar-post <?php echo ($current_percentage!=null&&$current_percentage==100?"viewed":"") ?>"></div>
                     <span class="lp-node-play"></span>
-                    <input class="knob knob-small" value="<?php echo ProfileComponentCompletedStatusService::getInstance()->getCompletedStatus($profile->getId(), $chapter->getId()) ?>" data-fgColor="#fff" data-bgColor="#000" data-width="24" data-thickness=".24" data-skin="" data-angleOffset=-5 data-readOnly=true data-displayInput=false >
+                    <input class="knob knob-small" value="<?php echo $current_percentage ?>" data-fgColor="#fff" data-bgColor="#000" data-width="24" data-thickness=".24" data-skin="" data-angleOffset=-5 data-readOnly=true data-displayInput=false >
                 </div>
                 <?php echo $chapter->getName() ?>
                 <span class="lp-time">10'32''</span>
             </a>
         </li>
+            <?php $previous_percentage = $current_percentage; ?>
         <?php endforeach ?>
     </ul>
 </div>
