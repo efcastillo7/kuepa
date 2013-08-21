4c4de614fcf26464d62805226f03cfc5e92df0dd
Carolina Bravo <sloopy@fiberbunny.com>
2013-08-21 17:36:19 -0300

    schema  for calendar

---
+++ lib/filter/doctrine/CalendarEventFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * CalendarEvent filter form.
+ *
+ * @package    kuepa
+ * @subpackage filter
+ * @author     fiberbunny
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class CalendarEventFormFilter extends BaseCalendarEventFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/MessageRecipientForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * MessageRecipient form.
+ *
+ * @package    kuepa
+ * @subpackage form
+ * @author     fiberbunny
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class MessageRecipientForm extends BaseMessageRecipientForm
+{
+  public function configure()
+  {
+  }
+}
