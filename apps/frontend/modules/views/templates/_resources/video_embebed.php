d0bf984054e5be2dac71db5efa7a2774e7ff1572
Pablo <pablo@kibind.com>
2013-09-09 22:30:10 -0300

    embebd video

---
+++ apps/frontend/modules/views/templates/_resources/video_embebed.php
@@ -0,0 +1,6 @@
+<video id="example_video_1" class="video-js vjs-default-skin"
+  controls preload="auto" width="auto" height="470"
+  data-setup='{"example_option":true}'>
+ <source src="/uploads/videos/rec_18854.mp4" type='video/mp4' />
+ <source src="/uploads/videos/rec_18854.ogg" type='video/ogg' />
+</video>
--- apps/frontend/modules/views/templates/_resources/video_vimeo.php
+++ apps/frontend/modules/views/templates/_resources/video_vimeo.php
@@ -2,7 +2,3 @@
 <video id="vid1" src="" class="video-js vjs-default-skin" controls preload="auto" width="auto" height="360" data-setup='{ "techOrder": ["vimeo"], "src": "<?php echo $resource->getContent() ?>", "loop": true, "autoplay": false }'>
   <p>Video Playback Not Supported</p>
 </video>
-<!-- <video id="example_video_1" class="video-js vjs-default-skin" preload="none" width="auto" height="360"
-    data-setup='{"techOrder":["vimeo"],"ytcontrols":false}'>
-  <source src="<?php echo $resource->getContent() ?>" type='video/vimeo' />
-</video> -->
