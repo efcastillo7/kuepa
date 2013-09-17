816c1ac8bda76ff5b7c7608e009d5c8ed26d1f35
Pablo <pablo@kibind.com>
2013-09-17 16:47:34 -0300

    tinymce layout

--- web/js/uno/layout.js
+++ web/js/uno/layout.js
@@ -20,7 +20,19 @@ $(function() {
     });
 
     //init tinyMCE ONCE!
-    tinyMCE.init({
-        mode: "none"
+    tinymce.init({
+        mode: "none",
+        plugins: [
+            "advlist autolink lists link image charmap anchor",
+            "searchreplace visualblocks code fullscreen",
+            "insertdatetime media table contextmenu paste"
+        ],
+        relative_urls: false,
+        convert_urls: false,
+        remove_script_host : false,
+        menubar: "edit insert format view table",
+        toolbar1: "undo redo | styleselect | bold italic | link image media | code | fullscreen",
+        toolbar2: "alignleft aligncenter alignright alignjustify | bullist numlist outdent indent"
     });
+
 });
