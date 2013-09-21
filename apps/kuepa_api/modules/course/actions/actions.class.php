08662738c72ca250b9bf4c2009c68782b93a6129
Diego Mazzocchi <diego.mazzocchi@teracode.com>
2013-09-21 16:42:48 -0300

    Fix course creation

--- apps/kuepa_api/modules/course/actions/actions.class.php
+++ apps/kuepa_api/modules/course/actions/actions.class.php
@@ -8,7 +8,7 @@
  * @author     fiberbunny
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class courseActions extends sfActions {
+class courseActions extends kuepaActions {
 
     /**
      * POST /course
@@ -16,11 +16,22 @@ class courseActions extends sfActions {
      * @param sfRequest $request A request object
      */
     public function executeCreate(sfWebRequest $request) {
+        return $this->update($request);
+    }
+    
+    private function update(sfWebRequest $request) {
         try {
-            $form = new CourseForm;
-            $values = $request->getParameter($form->getName());
-
-            $form->bind($values);
+            $id = $request->getParameter("id");
+        
+            if( $id ) {
+                $form = new CourseForm(Course::getRepository()->find($id));
+            } else {
+                $form = new CourseForm;
+            }
+            
+            $form->setValidator('_csrf_token', new sfValidatorPass);
+            
+            $form->bind($request->getParameter($form->getName()),$request->getFiles($form->getName()));
 
             if (!$form->isValid()) {
                 throw new BadRequest;
@@ -31,10 +42,12 @@ class courseActions extends sfActions {
             $course = $form->save();
 
             //add to user
-            CourseService::getInstance()->addTeacher($course->getId(), $this->getProfile()->getId());
+            if(!$id) {
+                CourseService::getInstance()->addTeacher($course->getId(), $this->getProfile()->getId());
+            }
             
             $response['status'] = 'success';
-            $response['id']  = $course->getId();
+            $response['course_id']  = $course->getId();
             $response['uri'] = $this->getContext()->getRouting()->generate('course_get', Array('id' => $course->getId()));
 
             $this->getResponse()->setStatusCode(201);
@@ -43,7 +56,13 @@ class courseActions extends sfActions {
         } catch (BadRequest $e) {
             $this->getResponse()->setStatusCode(400);
 
-            return $this->renderText(json_encode(Array('status' => 'error')));
+            $response['form_errors'] = Array();
+            
+            foreach($form->getErrorSchema()->getErrors() as $field => $e) {
+                $response['form_errors'][$field] = strval($e);
+            }
+                    
+            return $this->renderText(json_encode($response));
         } catch (Exception $e) {
             $this->getResponse()->setStatusCode(500);
 
@@ -91,7 +110,7 @@ class courseActions extends sfActions {
      * @param sfRequest $request A request object
      */
     public function executeEdit(sfWebRequest $request) {
-        return $this->renderText('edit');
+        return $this->update($request);
     }
 
     /**
