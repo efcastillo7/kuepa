e9deffadc07c1710520d55558fe9a7cfbdc14e23
Pablo <pablo@kibind.com>
2013-09-25 21:07:41 -0300

    ajax form exercise

--- apps/frontend/modules/views/templates/_resources/resource_Exercise.php
+++ apps/frontend/modules/views/templates/_resources/resource_Exercise.php
@@ -3,8 +3,25 @@
 <h3><?php echo $exercise->getTitle() ?></h3>
 <p><?php echo $exercise->getDescription() ?></p>
 
-
+<form action="<?php echo url_for("exercise/validate") ?>" method="post" id="exercise_form">
 <?php foreach ($exercise->getQuestions() as $question): ?>
   <hr>
   <?php include_partial("views/exercise/type_" . $question->getType(), array('exercise' => $exercise, 'question' => $question))?>
 <?php endforeach ?>
+<input type="hidden" name="exercise_id" value="<?php echo $exercise->getId() ?>">
+<input type="submit" value="Validar">
+</form>
+
+<script>
+	var options = {
+        success: function(data, statusText, xhr, $form) {
+            
+        },
+        dataType: 'json'
+	};
+
+	$("#exercise_form").ajaxForm(options);
+	$('#exercise_form').bind('form-pre-serialize', function(e) {
+        tinyMCE.triggerSave();
+    });
+</script>
