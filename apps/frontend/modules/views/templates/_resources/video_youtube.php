d0739f194045f5bc7dd145709b92903bec839b6d
Pablo <pablo@kibind.com>
2013-10-01 15:46:49 -0300

    added youtube controls

--- apps/frontend/modules/views/templates/_resources/video_youtube.php
+++ apps/frontend/modules/views/templates/_resources/video_youtube.php
@@ -1,5 +1,5 @@
 <?php use_javascript("/assets/video-js/plugins/vjs.youtube.js") ?>
 <video id="example_video_1" class="video-js vjs-default-skin" preload="none" width="auto" height="360"
-    data-setup='{"techOrder":["youtube"],"ytcontrols":false}'>
+    data-setup='{"techOrder":["youtube"],"ytcontrols":true}'>
   <source src="<?php echo $resource->getContent() ?>" type='video/youtube' />
 </video>
