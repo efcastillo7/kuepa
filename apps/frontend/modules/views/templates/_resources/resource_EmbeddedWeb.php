6ee41d30501319f9bc52d44a1d8512b55eafcf5e
Pablo <pablo@kibind.com>
2013-10-10 15:01:31 -0300

    migration

--- apps/frontend/modules/views/templates/_resources/resource_EmbeddedWeb.php
+++ apps/frontend/modules/views/templates/_resources/resource_EmbeddedWeb.php
@@ -1,1 +1,6 @@
 <iframe src="<?php echo $resource->getContent() ?>" frameborder="0"></iframe>
+<p>Fuente: <?php echo $resource->getContent() ?></p>
+<p>Creado el: <?php echo $resource->getCreatedAt() ?></p>
+<?php if ($resource->getCreatedAt() != $resource->getUpdatedAt()): ?>
+	<p>Última Modificación: <?php echo $resource->getUpdatedAt() ?></p>	
+<?php endif ?>
--- lib/service/ResourceService.php
+++ lib/service/ResourceService.php
@@ -10,6 +10,22 @@ class ResourceService {
         }
 
         return self::$instance;
-    }    
+    }
+
+    public function create($values = array()) {
+        $resource = ComponentService::getInstance()->create(Resource::TYPE, $values);
+
+        return $resource;
+    }
+
+    public function edit($resource_id, $values = array()) {
+        //check if course
+        return ComponentService::getInstance()->edit($resource_id, $values);
+    }
+
+    public function delete($resource_id) {
+        //check if course
+        return ComponentService::getInstance()->delete($resource_id);
+    }
 
 }
