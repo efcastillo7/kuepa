8f46dc1ca15fda8c6edbee9130d6f02f5e643ea9
Pablo <pablo@kibind.com>
2013-10-15 11:04:40 -0300

    fix preview/fwd lesson

--- lib/model/doctrine/Lesson.class.php
+++ lib/model/doctrine/Lesson.class.php
@@ -16,15 +16,19 @@ class Lesson extends BaseLesson {
     private $_actual_resource_position = null;
     private $_resource_list_id = array();
 
-    public function setActualResource($resource_id){
-        $this->_actual_resource = $resource_id;
-
-        //get resources as list id
+    private function initResources(){
         $resources = $this->getResources();
         $this->_resource_list_id = array();
         foreach ($resources as $id => $obj) {
             $this->_resource_list_id[] = $obj->getId();
         }
+    }
+
+    public function setActualResource($resource_id){
+        $this->_actual_resource = $resource_id;
+
+        //get resources as list id
+        $this->initResources();
 
         //update position
         if (!($this->_actual_resource_position = array_search($resource_id, $this->_resource_list_id))){
@@ -35,6 +39,10 @@ class Lesson extends BaseLesson {
     }
 
     public function getActualResourceId(){
+        if($this->_actual_resource_position == null){
+            $this->initResources();
+            $this->_actual_resource_position = 0;
+        }
         return $this->_resource_list_id[$this->_actual_resource_position];
     }
 
