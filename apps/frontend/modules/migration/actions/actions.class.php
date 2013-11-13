4f3e5f31d99c7d9e988c6a13fb187bfafc82f68d
Pablo <pablo@kibind.com>
2013-11-13 10:09:35 -0300

    changes

--- apps/frontend/modules/migration/actions/actions.class.php
+++ apps/frontend/modules/migration/actions/actions.class.php
@@ -19,7 +19,7 @@ class migrationActions extends sfActions
 		$quiz = $request->getParameter("quiz");
 		$lesson_id = $request->getParameter("to");
 
-		$mysql_conn = mysqli_connect("127.0.0.1","root","lalala182","z110k_$course");
+		$mysql_conn = mysqli_connect("127.0.0.1","root","p4ssw0rd123","z110k_$course");
     	mysqli_query($mysql_conn, "SET NAMES utf8;");
 
 
@@ -189,7 +189,7 @@ class migrationActions extends sfActions
   {
   	$this->setTemplate("index2");
 
-    $mysql_conn = mysqli_connect("127.0.0.1","root","lalala182","110k_dokeos_main");
+    $mysql_conn = mysqli_connect("127.0.0.1","root","p4ssw0rd123","110k_dokeos_main");
     mysqli_query($mysql_conn, "SET NAMES utf8;");
 
     $course_code = $request->getParameter("course");
@@ -334,144 +334,144 @@ class migrationActions extends sfActions
 	$this->unidades = $unidades;
 
 	//check if dir exists
-	// $base_path =  sfConfig::get('sf_upload_dir') . "/resources";
-	// if(!file_exists($base_path)){
-	// 	mkdir($base_path);
-	// }
+	$base_path =  sfConfig::get('sf_upload_dir') . "/resources";
+	if(!file_exists($base_path)){
+		mkdir($base_path);
+	}
 
-	// $base_path .= "/$course_code";
-	// if(!file_exists($base_path)){
-	// 	mkdir($base_path);
-	// }
+	$base_path .= "/$course_code";
+	if(!file_exists($base_path)){
+		mkdir($base_path);
+	}
 
-	// //images path
-	// if(!file_exists($base_path . "/images")){
-	// 	mkdir($base_path . "/images");
-	// }
+	//images path
+	if(!file_exists($base_path . "/images")){
+		mkdir($base_path . "/images");
+	}
 
-	// //video path
-	// if(!file_exists($base_path . "/videos")){
-	// 	mkdir($base_path . "/videos");
-	// }
+	//video path
+	if(!file_exists($base_path . "/videos")){
+		mkdir($base_path . "/videos");
+	}
 
 
-	// $base_path .= "/";
+	$base_path .= "/";
 
-	// $profile_id = $this->getUser()->getProfile()->getId();
+	$profile_id = $this->getUser()->getProfile()->getId();
 
-	// $course = CourseService::getInstance()->create(array(
-	// 	'name' => $course_code,
-	// 	'descripcion' => "$course_code",
-	// 	'profile_id' => $profile_id
-	// ));
+	$course = CourseService::getInstance()->create(array(
+		'name' => $course_code,
+		'descripcion' => "$course_code",
+		'profile_id' => $profile_id
+	));
 
-	// //add dummy user for testing
-	// CourseService::getInstance()->addTeacher($course->getId(), $profile_id);
+	//add dummy user for testing
+	CourseService::getInstance()->addTeacher($course->getId(), $profile_id);
 	
 
-	// foreach ($unidades as $unidad) {
-	// 	$chapter = ChapterService::getInstance()->create(array(
-	// 		'name' => $unidad['nombre'],
-	// 		'profile_id' => $profile_id
-	// 	));
+	foreach ($unidades as $unidad) {
+		$chapter = ChapterService::getInstance()->create(array(
+			'name' => $unidad['nombre'],
+			'profile_id' => $profile_id
+		));
 
-	// 	CourseService::getInstance()->addChapterToCourse($course->getId(), $chapter->getId());
+		CourseService::getInstance()->addChapterToCourse($course->getId(), $chapter->getId());
 
-	// 	foreach ($unidad['lessons'] as $lesson) {
-	// 		$nlesson = LessonService::getInstance()->create(array(
-	// 			'name' => $lesson['nombre'],
-	// 			'description' => $lesson['descripcion'],
-	// 			'profile_id' => $profile_id
-	// 		));
+		foreach ($unidad['lessons'] as $lesson) {
+			$nlesson = LessonService::getInstance()->create(array(
+				'name' => $lesson['nombre'],
+				'description' => $lesson['descripcion'],
+				'profile_id' => $profile_id
+			));
 
-	// 		//add lesson to chapter
-	// 		ChapterService::getInstance()->addLessonToChapter($chapter->getId(), $nlesson->getId());
+			//add lesson to chapter
+			ChapterService::getInstance()->addLessonToChapter($chapter->getId(), $nlesson->getId());
 
-	// 		//add resources
-	// 		foreach ($lesson['recursos'] as $recurso) {
-	// 			//set resource
-	// 			$resource = ResourceService::getInstance()->create(array(
-	// 				'name' => $recurso['nombre'],
-	// 				'description' => $recurso['data']['titulo'],
-	// 				'profile_id' => $profile_id
-	// 			));
+			//add resources
+			foreach ($lesson['recursos'] as $recurso) {
+				//set resource
+				$resource = ResourceService::getInstance()->create(array(
+					'name' => $recurso['nombre'],
+					'description' => $recurso['data']['titulo'],
+					'profile_id' => $profile_id
+				));
 
-	// 			//set resource data
-	// 			switch ($recurso['tipo']) {
-	// 				case 9:
-	// 					foreach ($recurso['data']['links'] as $link) {
-	// 						$pos = strrpos($link,"/");
-	// 						$imgname = str_replace(
-	// 							array(" ", "%", "/", "\""),
-	// 							array("-", "-", "-", "-"), 
-	// 							substr($link, $pos+1));
+				//set resource data
+				switch ($recurso['tipo']) {
+					case 9:
+						foreach ($recurso['data']['links'] as $link) {
+							$pos = strrpos($link,"/");
+							$imgname = str_replace(
+								array(" ", "%", "/", "\""),
+								array("-", "-", "-", "-"), 
+								substr($link, $pos+1));
 
-	// 						if(strpos($link,"http") === false){
-	// 							$link = "http://www.kuepa.com" . $link;
-	// 						}
-	// 						if(!file_exists($base_path . "/images/" . $imgname))
-	// 							copy($link, $base_path . "/images/" . $imgname);
-	// 					}
+							if(strpos($link,"http") === false){
+								$link = "http://www.kuepa.com" . $link;
+							}
+							if(!file_exists($base_path . "/images/" . $imgname))
+								copy($link, $base_path . "/images/" . $imgname);
+						}
 
 
-	// 					$rec = new ResourceDataText();
-	// 					$rec->setResourceId($resource->getId())
-	// 						->setContent($recurso['data']['texto'])
-	// 						->setEnabled(true)
-	// 						->setDuration(1)
-	// 						->save();
+						$rec = new ResourceDataText();
+						$rec->setResourceId($resource->getId())
+							->setContent($recurso['data']['texto'])
+							->setEnabled(true)
+							->setDuration(1)
+							->save();
 
-	// 					break;
-	// 				case 3:
-	// 					foreach ($recurso['data']['links'] as $link) {
-	// 						$imgname = str_replace(
-	// 							array(" ", "%", "/", "\""),
-	// 							array("-", "-", "-", "-"), 
-	// 							$link);
+						break;
+					case 3:
+						foreach ($recurso['data']['links'] as $link) {
+							$imgname = str_replace(
+								array(" ", "%", "/", "\""),
+								array("-", "-", "-", "-"), 
+								$link);
 
-	// 						$link = "http://www.kuepa.com/escuela/video/" . $link;
+							$link = "http://www.kuepa.com/escuela/video/" . $link;
 
-	// 						if(!file_exists($base_path . "videos/" . $imgname))
-	// 							copy($link, $base_path . "videos/" . $imgname);
-	// 					}
+							if(!file_exists($base_path . "videos/" . $imgname))
+								copy($link, $base_path . "videos/" . $imgname);
+						}
 
-	// 					$rec = new ResourceDataVideo();
+						$rec = new ResourceDataVideo();
 
-	// 					$rec->setResourceId($resource->getId())
-	// 						->setContent("/uploads/resources/$course_code/videos/" . $recurso['data']['links'][0])
-	// 						->setEnabled(true)
-	// 						->setDuration(1)
-	// 						->setVideoType('embebed')
-	// 						->save();
-	// 					break;
+						$rec->setResourceId($resource->getId())
+							->setContent("/uploads/resources/$course_code/videos/" . $recurso['data']['links'][0])
+							->setEnabled(true)
+							->setDuration(1)
+							->setVideoType('embebed')
+							->save();
+						break;
 					
-	// 				default:
-	// 					if($recurso['data']['embebido'] == 1){
-	// 						$rec = new ResourceDataVideo();
+					default:
+						if($recurso['data']['embebido'] == 1){
+							$rec = new ResourceDataVideo();
 
-	// 						$rec->setResourceId($resource->getId())
-	// 							->setContent($recurso['data']['links'][0])
-	// 							->setEnabled(true)
-	// 							->setDuration(1)
-	// 							->setVideoType('youtube')
-	// 							->save();
-	// 					}else{
-	// 						$rec = new ResourceDataEmbeddedWeb();
+							$rec->setResourceId($resource->getId())
+								->setContent($recurso['data']['links'][0])
+								->setEnabled(true)
+								->setDuration(1)
+								->setVideoType('youtube')
+								->save();
+						}else{
+							$rec = new ResourceDataEmbeddedWeb();
 
-	// 						$rec->setResourceId($resource->getId())
-	// 							->setContent($recurso['url'])
-	// 							->setEnabled(true)
-	// 							->setDuration(1)
-	// 							->save();
-	// 					}
-	// 					break;
-	// 			}
+							$rec->setResourceId($resource->getId())
+								->setContent($recurso['url'])
+								->setEnabled(true)
+								->setDuration(1)
+								->save();
+						}
+						break;
+				}
 
-	// 			//add reource to lesson
-	// 			LessonService::getInstance()->addResourceToLesson($nlesson->getId(), $resource->getId());
-	// 		}
-	// 	}
-	// }
+				//add reource to lesson
+				LessonService::getInstance()->addResourceToLesson($nlesson->getId(), $resource->getId());
+			}
+		}
+	}
 
 	//crear curso
 	//crear unidades
--- apps/frontend/modules/stats/templates/timelineSuccess.php
+++ apps/frontend/modules/stats/templates/timelineSuccess.php
@@ -1,38 +1,12 @@
 <?php use_stylesheet("/assets/timeline/css/default.css") ?>
 <?php use_stylesheet("/assets/timeline/css/component.css") ?>
-<?php use_helper("Date") ?>
 <h1>Camino de Aprendizaje</h1>
 <div class="row-fluid">
 	<div class="main span10">
 		<ul class="cbp_tmtimeline">
-			<?php foreach ($logs as $log): 
-				$resource = $log->getComponent();
-				$lesson = $resource->getParents()->getFirst();
-				$chapter = $lesson->getParents()->getFirst();
-				$course = $chapter->getParents()->getFirst();
-			?>
-			<li class="<?php echo $log->isSkim() ? "skim-resource" : "" ?>">
-				<time class="cbp_tmtime" datetime="<?php echo $log->getCreatedAt() ?>"><span><?php echo format_date($log->getCreatedAt(), "d") ?></span> <span><?php echo format_date($log->getCreatedAt(), "t") ?></span></time>
-				<div class="cbp_tmicon cbp_tmicon-phone"></div>
-				<div class="cbp_tmlabel bg-<?php echo $course->getColor()?>">
-					<div class="row-fluid">
-						<div class="span1"><img src="<?php echo $course->getThumbnailPath() ?>"/></div>
-						<div class="span9">
-							<span><?php echo $course->getName() ?> &gt; <?php echo $chapter->getName() ?> &gt; <?php echo $lesson->getName() ?> </span>
-							<h2><?php echo $resource->getName() ?></h2>
-						</div>
-						<div class="span2">
-							Dedicado: <br>
-							<?php echo gmdate("H:i:s", $log->getLapse())?>
-						</div>
-					</div>				
-				</div>
-			</li>
-			<!-- <li class="spacer">
-				<div style="height: <?php echo $log->getLapse()?>px;"></div>
-			</li> -->
-			<?php endforeach ?>
+			<?php include_partial("timeline_points", array('logs' => $logs)) ?>
 		</ul>
+		<div id="more">Ver más</div>
 	</div>
 	<div class="span2">
 		<a href="#" id="hide-skim">Ocultar/Mostrar Lecturas Rápidas</a>
@@ -40,9 +14,33 @@
 </div>
 
 <script>
+	var last_date = null;
 	$(document).ready(function(){
 		$("#hide-skim").click(function(){
 			$(".skim-resource").toggle();
 		});
+
+		$.ajax({
+			url: '<?php echo url_for("stats/timeline") ?>',
+			data: {count: 10, to: last_date},
+			dataType: 'json',
+			success: function(data, textStatus, jqXHR){
+				$(".cbp_tmtimeline").append(data.template);
+				last_date = data.last_date;
+			}
+		});
+
+		$("#more").click(function(){
+			$.ajax({
+				url: '<?php echo url_for("stats/timeline") ?>',
+				data: {count: 10, to: last_date},
+				dataType: 'json',
+				success: function(data, textStatus, jqXHR){
+					$(".cbp_tmtimeline").append(data.template);
+					last_date = data.last_date;
+				}
+			});
+		});
+
 	});
 </script>
