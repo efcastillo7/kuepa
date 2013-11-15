d31362e8754eb9ab2bbbec38ffa1361492955452
Pablo <pablo@kibind.com>
2013-11-15 11:12:45 -0300

    refactor + end screen loads timeline

--- apps/frontend/modules/stats/templates/timelineSuccess.php
+++ apps/frontend/modules/stats/templates/timelineSuccess.php
@@ -6,7 +6,7 @@
 		<ul class="cbp_tmtimeline">
 			<?php include_partial("timeline_points", array('logs' => $logs)) ?>
 		</ul>
-		<div id="more">Ver más</div>
+		<a id="more" href="javascript:void(0)">Ver más</a>
 	</div>
 	<div class="span2">
 		<a href="#" id="hide-skim">Ocultar/Mostrar Lecturas Rápidas</a>
@@ -15,32 +15,53 @@
 
 <script>
 	var last_date = null;
+	var reached_last = false;
+	var loading = false;
+
 	$(document).ready(function(){
 		$("#hide-skim").click(function(){
 			$(".skim-resource").toggle();
 		});
 
-		$.ajax({
-			url: '<?php echo url_for("stats/timeline") ?>',
-			data: {count: 10, to: last_date},
-			dataType: 'json',
-			success: function(data, textStatus, jqXHR){
-				$(".cbp_tmtimeline").append(data.template);
-				last_date = data.last_date;
+		function loadHistory(){
+			if(reached_last || loading){
+				return;
 			}
-		});
 
-		$("#more").click(function(){
+			loading = true;
+
 			$.ajax({
 				url: '<?php echo url_for("stats/timeline") ?>',
 				data: {count: 10, to: last_date},
 				dataType: 'json',
 				success: function(data, textStatus, jqXHR){
-					$(".cbp_tmtimeline").append(data.template);
+					console.log(data);
+					loading = false;
+
+					if(data.template != ""){
+						$(".cbp_tmtimeline").append(data.template);	
+					}else{
+						reached_last = true;
+						$("#more").hide();
+					}
+					
 					last_date = data.last_date;
+				},
+				error: function(data, textStatus, jqXHR){
+					$("#more").hide();
 				}
 			});
+		}
+
+		// loadHistory();
+
+		$(window).scroll(function () {
+		   if ($(window).scrollTop() >= $(document).height() - $(window).height() - 10) {
+		      loadHistory();
+		   }
 		});
+
+		$("#more").click(loadHistory);
 
 	});
 </script>
