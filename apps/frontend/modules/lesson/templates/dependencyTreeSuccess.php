269586e5279361602bdf9ebb60931dbc21874086
Francisco Fabian Ruiz Guerrero <franciscofruiz@hotmail.com>
2014-01-08 10:56:22 -0500

    modificado layout, vertical

--- apps/frontend/modules/lesson/templates/dependencyTreeSuccess.php
+++ apps/frontend/modules/lesson/templates/dependencyTreeSuccess.php
@@ -1,32 +1,35 @@
 <div class="container">
 <?php echo link_to("Listado de Lecciones",'lesson/list') ?> <br><br>
 <h3>Dependencia de Lecciones</h3>
-<h5>Leccion :</h5>
 <form class="form" id="dependency_tree_form" name="dependency" action="<?php echo url_for('lesson/saveDependency') ?>" method="post">
+<div class="row">
+  <div class="span4">
+    <h5>Leccion :</h5>
 
-<div class="form-inline" id="lesson_origin">
-  <label>Curso </label>
-   <select name="dependency_path[course_id]" id="dependency_path_course_id">
-    <?php foreach ($courses as $key => $c) { ?>
-      <option value="<?php echo $c->getId(); ?>"><?php echo $c->getName(); ?></option>
-    <?php } ?>
-  </select>
-  
-  <label>Unidad </label>
-  <select name="dependency_path[chapter_id]" id="dependency_path_chapter_id">
-    <option value="">Seleccionar...</option>
-  </select>
- 
-  <label>Lecci&oacute;n </label>
-  <select name="dependency_path[lesson_id]" id="dependency_path_lesson_id">
-    <option value="">Seleccionar...</option>
-  </select>
-</div>
+      <div class="" id="lesson_origin">
+        <label>Curso </label>
+         <select name="dependency_path[course_id]" id="dependency_path_course_id">
+          <?php foreach ($courses as $key => $c) { ?>
+            <option value="<?php echo $c->getId(); ?>"><?php echo $c->getName(); ?></option>
+          <?php } ?>
+        </select>
+        
+        <label>Unidad </label>
+        <select name="dependency_path[chapter_id]" id="dependency_path_chapter_id">
+          <option value="">Seleccionar...</option>
+        </select>
+       
+        <label>Lecci&oacute;n </label>
+        <select name="dependency_path[lesson_id]" id="dependency_path_lesson_id">
+          <option value="">Seleccionar...</option>
+        </select>
+      </div>
+ </div>
 
-<br >
-<h5>Depende de : </h5><br> 
+<div class="span4">
+  <h5>Depende de : </h5>
   <div class="row">
-    <div class="span3 form-inline">
+    <div class="span3">
       <label>Curso </label>
        <select name="dependency_path[depends_course_id]" id="dependency_path_depends_course_id">
         <?php foreach ($courses as $key => $c) { ?>
@@ -34,24 +37,28 @@
         <?php } ?>
       </select>
     </div>
-    <div class="span4 form-inline">
+    <div class="span4">
         <label>Unidad </label>
         <select name="dependency_path[depends_chapter_id]" id="dependency_path_depends_chapter_id">
           <option value="test">Seleccionar...</option>
         </select>
     </div>
   </div>
+</div>
+</div>
+
+
 
   <div class="row">
     <div class="span4">
-         <h6><b>Requisitos Seleccionados</b></h6>
+         <h6><b>Dependencias Seleccionadas</b></h6>
         <div id="dependency_path_list">
            Selecciona Curso, Unidad y Lecci&oacute;n...
         </div>
      </div>
 
     <div class="span4">
-        <h5>Agregar nuevos requisitos</h5>
+        <h5>Seleccion de dependencias</h5>
         <div id="depends_lesson_wrapper">
            Selecciona Curso y Unidad...
           <?php /*include_partial("depends_lesson_list", array('lessons' => $lessons))*/ ?>
