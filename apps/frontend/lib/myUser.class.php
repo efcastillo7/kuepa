8cb38075b79f7b08dfce268f57a5070fb503440c
iandrono <ivan@kibind.com>
2014-01-28 15:54:53 -0300

    Added signin and signout hooks to myUser class

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -1,19 +1,29 @@
 <?php
 
-class myUser extends sfGuardSecurityUser
-{
-	public function isValidAccount(){
-		//check if account is enabled - TODO: Add field
+class myUser extends sfGuardSecurityUser {
 
-		//check if account date is valid
-		$date = $this->getProfile()->getValidUntil();
-		if($date){
-			$valid_until = strtotime($date);
-			$diff = $valid_until - time();
+    public function isValidAccount() {
+        //check if account is enabled - TODO: Add field
+        //check if account date is valid
+        $date = $this->getProfile()->getValidUntil();
+        if ($date) {
+            $valid_until = strtotime($date);
+            $diff = $valid_until - time();
 
-			return $diff > 0;
-		}
-		
-		return true;
-	}
+            return $diff > 0;
+        }
+
+        return true;
+    }
+    
+    public function signIn($user, $remember = false, $con = null) {
+        //entra cuando hay login succesfull
+        //setear cultura de usuario, zona horaria, etc.
+        parent::signIn($user, $remember, $con);
+    }
+    
+    public function signOut() {
+        parent::signOut();
+    }
+
 }
