6bd72afb3850be624f829351864491f2c717e526
Pablo <pablo@kibind.com>
2014-02-07 18:12:37 -0300

    removed unnecesary

--- apps/frontend/modules/layout/templates/_footer.php
+++ apps/frontend/modules/layout/templates/_footer.php
@@ -4,11 +4,3 @@
     <p class="ft-copy">Copyright &copy; Kuepa 2014</p>
   </div>
 </footer>
-<script type="text/javascript">
-        $(".knob-small").knob({
-            height: 24
-        });
-        $(function(){
-           $('[rel="tooltip"]').tooltip();
-        });
-</script>
