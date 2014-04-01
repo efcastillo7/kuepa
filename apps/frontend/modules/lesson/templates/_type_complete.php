51360c3628534c484414ab9639fd424001865265
Pablo Rossi <info@pablorossi.info>
2014-04-01 13:06:49 -0300

    Ejercicios y Answers

--- apps/frontend/modules/lesson/templates/_type_complete.php
+++ apps/frontend/modules/lesson/templates/_type_complete.php
@@ -4,16 +4,16 @@
 </h4>
 <fieldset class="lssn-complete">
     <?php foreach ($question->getAnswers() as $answer): ?>
-    <?php 
-      $input = new sfWidgetFormInput(array(), array('class' => 'input-small orange'));
-      //input html
-      $input_txt = $input->render("exercise[" . $exercise->getId() . "][" . $question->getId() . "][]");
-      $input_txt .= "<span id='answer_" . $exercise->getId() . "_" . $question->getId() . "'></span>";
-      
-      //array_replace(array, array1)
-      $value = preg_replace('/\[(.*?)\]/i', $input_txt, $answer->getTitle()); 
-    ?>
-    <!-- make select -->
+      <?php 
+        $input = new sfWidgetFormInput(array(), array('class' => 'input-small orange'));
+        //input html
+        $input_txt = $input->render("exercise[" . $exercise->getId() . "][" . $question->getId() . "][]");
+        $input_txt .= "<span class='answer-item' id='answer_" . $exercise->getId() . "_" . $question->getId() . "'></span>";
+        $input_txt = "<div class='answer-container'>" . $input_txt . "</div>";
+        
+        //array_replace(array, array1)
+        $value = preg_replace('/\[(.*?)\]/i', $input_txt, $answer->getTitle()); 
+      ?>
     <?php endforeach ?>
     <?php echo $value ?>
 </fieldset>
