a17a4acdf68aaafcf005a04e803a9aa4d3b1d5c2
Pablo <pablo@kibind.com>
2014-04-01 21:00:52 -0300

    video embebd get from kuepa.com

--- apps/frontend/modules/views/templates/_resources/video_embebed.php
+++ apps/frontend/modules/views/templates/_resources/video_embebed.php
@@ -1,5 +1,5 @@
 <video id="video_embebd" class="video-js vjs-default-skin"
   controls preload="auto" width="auto" height="470"
   data-setup='{"example_option":true}'>
- <source src="<?php echo $resource->getContent() ?>" type='video/mp4' />
+ <source src="http://plataforma.kuepa.com<?php echo $resource->getContent() ?>" type='video/mp4' />
 </video>
