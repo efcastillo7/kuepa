63f89bd025d1113f7615446b1c14efb80a15c9ba
Pablo <pablo@kibind.com>
2014-04-04 13:51:55 -0300

    removed limit

--- lib/service/CourseService.php
+++ lib/service/CourseService.php
@@ -63,7 +63,6 @@ class CourseService {
                 ->innerJoin('sgug.Group sgg')
                 ->andWhere('sgg.name = ?', 'estudiantes')
                 ->andWhere('sgu.is_active = true')
-                ->limit(20)
                 ->execute();
 
         //users from college
@@ -77,7 +76,6 @@ class CourseService {
                 // ->innerJoin("c.CollegeLearningPath clp")
                 ->andWhere('sgu.is_active = true')
                 ->whereIn("pc.college_id", $colleges->getPrimaryKeys())
-                ->limit(20)
                 ->execute();
 
         return $users = $q1->merge($q2);
