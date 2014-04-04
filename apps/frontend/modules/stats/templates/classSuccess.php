1c44c5e1beb5ce6e98a9e91423bf39707e4990a0
Pablo <pablo@kibind.com>
2014-04-04 13:30:49 -0300

    little style

--- apps/frontend/modules/stats/templates/classSuccess.php
+++ apps/frontend/modules/stats/templates/classSuccess.php
@@ -1,21 +1,5 @@
 <?php use_helper("Date") ?>
 <?php use_helper("LocalDate") ?>
-<h2>
-	<?php echo $course->getName() ?>
-	<?php if ($group): ?>
-		> <?php echo $group->getName() ?>
-	<?php endif ?>
-</h2>
-<?php if ($groups): ?>
-<div>
-	<h4>Ver por Grupo</h4>
-		<div><a href="<?php echo url_for("stats/class?course_id=" . $course->getId()) ?>">Todos</a></div>
-		<?php foreach ($groups as $group): ?>
-			<div><a href="<?php echo url_for("stats/class?course_id=" . $course->getId() ."&group=" . $group->getId()) ?>"><?php echo $group->getName() ?></a></div>
-		<?php endforeach ?>
-</div>
-<?php endif ?>
-
 <style>
 	/*table{table-layout: fixed;}*/
 	/*table td{overflow: hidden;}*/
@@ -30,86 +14,117 @@
 </style>
 
 
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12">
+			<h5 class="HelveticaLt"><?php echo $course->getName() ?></h5>
+			<h3 class="HelveticaLt">
+			<?php if ($group): ?>
+				<?php echo $group->getName() ?>
+			<?php else: ?>
+				Todos
+			<?php endif; ?>
+			</h3>
 
-<table class="table table-hover table-responsive">
-	<thead>
-		<tr>
-			<td colspan="5"></td>
-			<?php foreach ($course->getChapters() as $chapter): ?>
-			<th colspan="2" class="bl bt"><?php echo $chapter->getName() ?></th>
-			<?php endforeach ?>
-		</tr>
-		<tr>
-			<th>Nombre alumno</th>
-			<th>Progreso</th>
-			<th>Tiempo dedicado</th>
-			<th>Última Conexión</th>
-			<th>Unidades Completadas</th>
+			<?php if ($groups): ?>
+			<div>
+				<h5 class="HelveticaLt">Ver por Grupo</h5>
+					<div><a href="<?php echo url_for("stats/class?course_id=" . $course->getId()) ?>">Todos</a></div>
+					<?php foreach ($groups as $group): ?>
+						<div><a href="<?php echo url_for("stats/class?course_id=" . $course->getId() ."&group=" . $group->getId()) ?>"><?php echo $group->getName() ?></a></div>
+					<?php endforeach ?>
+			</div>
+			<?php endif ?>
 
-			<!-- por unidad -->
-			<?php foreach ($course->getChapters() as $chapter): ?>
-			<th class="bl">Progreso</th>
-			<th>Tiempo Dedicado</th>
-			<?php endforeach ?>
-			<!-- fin por unidad -->
-		</tr>
-	</thead>
-	<tbody>
-		<?php $i=0; foreach ($students as $student): ?>
-		<tr>
-			<th><?php echo ++$i . " " . $student->getFullName() ?></th>
-			<td>
-				<div class="progress">
-					<?php if ($student->getComponentStatus($course->getId()) < 35): ?>
-					<div class="bar bar-danger" style="width: <?php echo $student->getComponentStatus($course->getId())?>%;"><?php echo $student->getComponentStatus($course->getId())?>%</div>	
-					<?php elseif ($student->getComponentStatus($course->getId()) < 70): ?>
-					<div class="bar bar-warning" style="width: <?php echo $student->getComponentStatus($course->getId())?>%;"><?php echo $student->getComponentStatus($course->getId())?>%</div>
-					<?php else: ?>
-					<div class="bar bar-success" style="width: <?php echo $student->getComponentStatus($course->getId())?>%;"><?php echo $student->getComponentStatus($course->getId())?>%</div>
-					<?php endif ?>
-				</div>
-			</td>
-			<td>
-				<?php 
-					$time_course = LogService::getInstance()->getTotalTimeByRoute($student->getId(), array('course_id' => $course->getId()));
-				if($time_course == 0):?>
-				-
-				<?php else: ?>
-				<?php echo gmdate("H:i:s", $time_course) ?>
-				<?php endif; ?>
-			</td>
-			<td><?php echo utcToLocalDate($student->getLastAccess(), "dd/M/yyyy H:mm") ?></td>
-			<td>
-				<?php echo ProfileComponentCompletedStatusService::getInstance()->getCompletedChilds($course->getId(), $student->getId()) ?>
-				/
-				<?php echo $course->getChapters()->count() ?>
-			</td>
 
-			<!-- por unidad -->
-			<?php foreach ($course->getChapters() as $chapter): $get_status = ($time_course > 0);?>
-			<td class="bl">
-				<?php if (!isset($status[$student->getId()]) || !isset($status[$student->getId()][$chapter->getId()])): ?>
-				<?php $get_status = false; ?>
-				-
-				<?php else: ?>
-				<?php echo $status[$student->getId()][$chapter->getId()] ?> %
-				<?php endif ?>
-			</td>
-			<td>
-				<?php if ($get_status){
-					$time = LogService::getInstance()->getTotalTimeByRoute($student->getId(), array('course_id' => $course->getId(), 'chapter_id' => $chapter->getId())); 
-				}else{ $time = 0;} ?>
-				<?php 
-				if($time == 0):?>
-				-
-				<?php else: ?>
-				<?php echo gmdate("H:i:s", $time) ?>
-				<?php endif; ?>
-			</td>
-			<?php endforeach ?>
-			<!-- fin por unidad -->
-		</tr>
-		<?php endforeach ?>
-	</tbody>
-</table>
+			<table class="table table-hover table-responsive">
+				<thead>
+					<tr>
+						<td colspan="5"></td>
+						<?php foreach ($course->getChapters() as $chapter): ?>
+						<th colspan="2" class="bl bt"><?php echo $chapter->getName() ?></th>
+						<?php endforeach ?>
+					</tr>
+					<tr>
+						<th>Nombre alumno</th>
+						<th>Progreso</th>
+						<th>Tiempo dedicado</th>
+						<th>Última Conexión</th>
+						<th>Unidades Completadas</th>
+
+						<!-- por unidad -->
+						<?php foreach ($course->getChapters() as $chapter): ?>
+						<th class="bl">Progreso</th>
+						<th>Tiempo Dedicado</th>
+						<?php endforeach ?>
+						<!-- fin por unidad -->
+					</tr>
+				</thead>
+				<tbody>
+					<?php $i=0; foreach ($students as $student): ?>
+					<tr>
+						<th><?php echo ++$i . " " . $student->getFullName() ?></th>
+						<td>
+							<div class="progress">
+								<?php if ($student->getComponentStatus($course->getId()) < 35): ?>
+								<div class="bar bar-danger" style="width: <?php echo $student->getComponentStatus($course->getId())?>%;"><?php echo $student->getComponentStatus($course->getId())?>%</div>	
+								<?php elseif ($student->getComponentStatus($course->getId()) < 70): ?>
+								<div class="bar bar-warning" style="width: <?php echo $student->getComponentStatus($course->getId())?>%;"><?php echo $student->getComponentStatus($course->getId())?>%</div>
+								<?php else: ?>
+								<div class="bar bar-success" style="width: <?php echo $student->getComponentStatus($course->getId())?>%;"><?php echo $student->getComponentStatus($course->getId())?>%</div>
+								<?php endif ?>
+							</div>
+						</td>
+						<td>
+							<?php 
+								$time_course = LogService::getInstance()->getTotalTimeByRoute($student->getId(), array('course_id' => $course->getId()));
+							if($time_course == 0):?>
+							-
+							<?php else: ?>
+							<?php echo gmdate("H:i:s", $time_course) ?>
+							<?php endif; ?>
+						</td>
+						<td><?php echo utcToLocalDate($student->getLastAccess(), "dd/M/yyyy H:mm") ?></td>
+						<td>
+							<?php echo ProfileComponentCompletedStatusService::getInstance()->getCompletedChilds($course->getId(), $student->getId()) ?>
+							/
+							<?php echo $course->getChapters()->count() ?>
+						</td>
+
+						<!-- por unidad -->
+						<?php foreach ($course->getChapters() as $chapter): $get_status = ($time_course > 0);?>
+						<td class="bl">
+							<?php if (!isset($status[$student->getId()]) || !isset($status[$student->getId()][$chapter->getId()])): ?>
+							<?php $get_status = false; ?>
+							-
+							<?php else: ?>
+							<?php echo $status[$student->getId()][$chapter->getId()] ?> %
+							<?php endif ?>
+						</td>
+						<td>
+							<?php if ($get_status){
+								$time = LogService::getInstance()->getTotalTimeByRoute($student->getId(), array('course_id' => $course->getId(), 'chapter_id' => $chapter->getId())); 
+							}else{ $time = 0;} ?>
+							<?php 
+							if($time == 0):?>
+							-
+							<?php else: ?>
+							<?php echo gmdate("H:i:s", $time) ?>
+							<?php endif; ?>
+						</td>
+						<?php endforeach ?>
+						<!-- fin por unidad -->
+					</tr>
+					<?php endforeach ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
 				
