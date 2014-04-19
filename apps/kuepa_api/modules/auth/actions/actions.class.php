d8432eb695b55aa4f2b592508fb7a33acef870f8
Pablo <pablo@kibind.com>
2014-04-19 19:11:12 -0300

    api login

--- apps/kuepa_api/modules/auth/actions/actions.class.php
+++ apps/kuepa_api/modules/auth/actions/actions.class.php
@@ -17,29 +17,26 @@ class authActions extends sfActions
   */
   public function executeLogin(sfWebRequest $request)
   {
-  	header('Access-Control-Allow-Origin: *');
-  	
-	$username = trim($request->getPostParameter("username", ""));
-	$password = trim($request->getPostParameter("password", ""));
+    header('Access-Control-Allow-Origin: *');
+    
+    $username = trim($request->getPostParameter("username", ""));
+    $password = trim($request->getPostParameter("password", ""));
 
-	if (!empty($username) && !empty($password) && $request->isMethod('post')){
-    // if (!empty($username) && !empty($password)){
-    	$user = Doctrine::getTable('sfGuardUser')->retrieveByUsername($username);
-    	if($user)
-        {
-	        // password is ok?
-	        if ($user->getIsActive() && $user->checkPassword($password))
-	        {
-	        	$this->getUser()->signin($user, false);
-	            $this->getResponse()->setStatusCode(200);
-	            return $this->renderText(json_encode(array('status' => 'ok')));
-	        }
-	    }
+    if (!empty($username) && !empty($password) && $request->isMethod('post')){
+        $user = ProfileService::getInstance()->isValidUser($username,$password);
+        if($user)
+          {
+            $this->getUser()->signin($user);
+            $this->getResponse()->setStatusCode(200);
+            
+            return $this->renderText(json_encode(array('status' => 'ok')));
+          }
+        }
+      }
+
+      $this->getResponse()->setHeaderOnly(true);
+      $this->getResponse()->setStatusCode(401);
+
+      return sfView::NONE;
     }
-
-    $this->getResponse()->setHeaderOnly(true);
-    $this->getResponse()->setStatusCode(401);
-
-    return sfView::NONE;
-  }
 }
