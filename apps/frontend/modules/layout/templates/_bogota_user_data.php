1b88d330ac2d9e1bae617d37cb091dd57fab9b1a
Pablo <pablo@kibind.com>
2014-05-06 20:50:38 -0300

    fixed js

--- apps/frontend/modules/layout/templates/_bogota_user_data.php
+++ apps/frontend/modules/layout/templates/_bogota_user_data.php
@@ -23,7 +23,7 @@
 		$("#send_data").click(function(){
 			var phone = $("#phone").val();
 			var celphone = $("#celphone").val();
-			$.ajax('/kuepa_api_dev.php/profile/1', { 
+			$.ajax('/kuepa_api_dev.php/profile/0', { 
 				type: 'POST', 
 				success: function(response){ 
 					$.ajax('/kuepa_api_dev.php/profile/flashmessage',{
@@ -34,13 +34,23 @@
 						type: 'POST'
 					});
 				}, 
-				data: { profile: { phone: phone, cellphone: celphone } },
+				data: { profile: { phone: phone, celphone: celphone } },
 				error: function(response){
 					alert(response.responseText)
 				},
 				dataType: 'json'
 			});
-		})
+		});
+
+		$("#cancel").click(function(){
+			$.ajax('/kuepa_api_dev.php/profile/flashmessage',{
+				data: { id: <?php echo $message->getId() ?> },
+				success: function(){
+					magnificPopup.close();
+				},
+				type: 'POST'
+			});
+		});
 	});
 
 
@@ -51,51 +61,29 @@
 		<div class="container">
 			<div class="row">
 				<div class="slide" data-slide="1">
-				  	<p class="slide-title">
-				  		¡Hola <?php echo $profile->getFirstName() ?>! 
+					<p class="slide-title">
+				  		4. ¡A estudiar!
+				  	</p>
+				  	<p class="slide-main">
+				  		Y listo, basta de historias, ya puedes comenzar. <br><br>
+				  		Para que te mantengamos al tanto de los concursos y premios por usar y estudiar en la plataforma, déjanos los siguientes datos:
 				  	</p>
 				  	<div class="slide-main">
 				  		<p>
-				  		Bienvenido a la formación virtual del proyecto de Fortalecimiento de Estándares de Competencias 2014. En este tutorial podrás ver el funcionamiento de la plataforma. 
-				  		</p>
-				  		<p>
-				  			Para continuar te pedimos que completes los siguientes campos:
 				  			<form action="">
-								Teléfono: <br>
+								Email: <br>
 				  				<input id="phone" type="text" class="input-big">
 				  				<br>
 				  				<div>Celular:</div>
 				  				<input id="celphone" type="text" class="input-big">
 				  				<br>
-				  				<input id="send_data" type="button" class="btn btn-primary btn-large" value="Confirmar">
+				  				<input id="send_data" type="button" class="btn btn-warning btn-large" value="¡Vale, quiero participar!">
+				  				<input id="cancel" type="button" class="btn btn-primary btn-large" value="No, gracias">
 				  			</form>
 				  		</p>
 				  	</div>
 				</div>
 			</div>
-		</div>
-		<div class="container">
-			<div class="row">
-				<div class="slide slide3 slide-small" data-slide="2">
-					<div class="arrow"></div>
-					<p class="slide-title">
-				  		1. Tu camino de aprendizaje
-				  	</p>
-				  	<p class="slide-main">
-				  		Con tus resultados te asignaremos las lecciones que debes tomar para estar listo para la prueba, siempre podrás ver cuáles te faltan aquí.
-				  	</p>
-				  	<a href="javascript:void(0)" class="btn btn-primary">¡Listo! Próxima...</a>
-				</div>
-			</div>
-		</div>
-		<div class="slide slide4 slide-small" data-slide="3">
-					<p class="slide-title">
-				  		2. Explora por ti mismo
-				  	</p>
-				  	<p class="slide-main">
-				  		Si quieres ver las lecciones disponibles y conocer todo el contenido ve a cada materia y navega libremente.
-				  	</p>
-				  	<a href="" id="goto-lesson" class="btn btn-primary">¡Listo! Próxima...</a>
 		</div>
 	</div>
 </div>
